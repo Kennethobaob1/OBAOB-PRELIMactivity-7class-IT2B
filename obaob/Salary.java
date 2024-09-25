75e5b642ffcfbd54e3181fce10f385755686ea65
Kennethobaob1 <kennethobaob1@gmail.com>
2024-09-25 10:52:20 +0800

    Add files via upload

---
+++ obaob/Salary.java
@@ -0,0 +1,28 @@
+package obaob;
+
+
+public class Salary {
+      String id;
+    String name;
+    double rate;
+    int hoursWorked;
+    double deduction;
+    
+    Salary (String id, String name, double rate, int hoursWorked, double deduction) {
+        this.id = id;
+        this.name = name;
+        this.rate = rate;
+        this.hoursWorked = hoursWorked;
+        this.deduction = deduction;
+    }
+    // Method to calculate the gross salary
+    double calculateGross() {
+        return rate * hoursWorked;
+    }
+    // Method to calculate the net pay
+    double calculateNetPay() {
+        return calculateGross() - deduction;
+    }
+   
+}
+
